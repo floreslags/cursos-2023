7fe56a8a6ccc7bb4f93855319990fd18f2f20017
sergioflags <sergiofol1093@gmail.com>
2023-10-26 13:59:17 -0600

    React: Fundamentos - Componentes

--- Frontend/Desarrollo/react/react-basics/src/App.js
+++ Frontend/Desarrollo/react/react-basics/src/App.js
@@ -1,29 +1,14 @@
+import React from 'react'
 import logo from './logo.svg';
+import Componente from './components/Componente';
 import './App.css';
 
 function App() {
-  let name = 'Sergio';
 
-  // Operador ternario
-
-  let auth = true;
-
-  let seasons = ["spring","summer","autumn","winter"];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <label htmlFor="name">name</label>
-        <input type="text" id="name"/>
-        <h1>{name}</h1>
-        <p>{auth?"The user is logged in":"The user is loged out"}</p>
-        <p>{2+1}</p>
-
-        <ul>
-          {seasons.map((el,index)=>(
-            <li key={index}>{el}</li>
-          ))}
-        </ul>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -36,6 +21,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <section>
+        <Componente msg="Hola Soy un Componente funcional expresado desde una prop" />
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+      </section>
     </div>
   );
 }
