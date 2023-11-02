f328e1e3ae7746c966277937b90ebe68c091be85
sergioflags <sergiofol1093@gmail.com>
2023-11-02 16:14:22 -0600

    React: Fundamentos - Estilos

--- Frontend/Desarrollo/react/react-basics/src/App.js
+++ Frontend/Desarrollo/react/react-basics/src/App.js
@@ -17,6 +17,7 @@ import HooksAjax from './components/HooksAjax';
 import HooksCustom from './components/HooksCustom';
 import References from './components/References';
 import Forms from './components/Forms';
+import Styles from './components/Styles';
 
 function App() {
 
@@ -82,6 +83,8 @@ function App() {
         <References/>
         <hr/>
         <Forms/>
+        <hr/>
+        <Styles/>
       </section>
       </header>
     </div>
