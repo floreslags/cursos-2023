cbb87bc704fef6e9ac36610d459e995b12f8ecfd
sergioflags <sergiofol1093@gmail.com>
2023-11-23 13:31:18 -0600

    React: Fundamentos - Ejercicio: CRUDApp Simple

---
+++ Frontend/Desarrollo/react/react-exercises/src/App.js
@@ -0,0 +1,13 @@
+import { CrudApp } from "./components/CrudApp";
+
+function App() {
+  return (
+    <>
+      <h1>React Exercises</h1>
+      <CrudApp></CrudApp>
+      <hr/>
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/Desarrollo/react/react-exercises/src/components/CrudTable.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { CrudTableRow } from './CrudTableRow';
+
+export const CrudTable = ({data,setDataToEdit,deleteData}) => {
+  return (
+    <div>
+        <h3>Data Table</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Constelación</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.length === 0?
+                (
+                    <tr>
+                        <td colSpan="3">Sin Datos</td>
+                    </tr>
+                ):data.map(el=><CrudTableRow el={el} key={el.id} setDataToEdit={setDataToEdit} deleteData={deleteData}/>)
+                }
+            </tbody>
+        </table>
+    </div>
+  )
+}
